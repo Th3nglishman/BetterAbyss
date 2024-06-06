84c78ad11e20703156e7fa41c514666b315085c8
John Whitney <50185477+Th3nglishman@users.noreply.github.com>
2024-06-06 15:11:03 -0700

    Update BootyfulSoup.py

--- BootyfulSoup.py
+++ BootyfulSoup.py
@@ -1,8 +1,1 @@
-import urllib.request
-import requests
-from bs4 import BeautifulSoup
 
-
-def get_soup():
-    result = urllib.request.urlopen("https://leagueoflegends.fandom.com/wiki/Module:ChampionData/data?action=edit")
-    return str(result.read())
